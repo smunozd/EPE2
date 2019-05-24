b917c452c58fb222bd46b1ee4984480bd9648d65
cristianloyola <48607084+cristianloyola@users.noreply.github.com>
2019-05-24 03:24:08 -0400

    Create safeurl.pipe.ts
    permitir variable URL

---
+++ safeurl.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+@Pipe({
+  name: 'safeUrl'
+})
+export class safeUrlPipe implements PipeTransform {
+  constructor ( private _domSanitizer:DomSanitizer){}
+  transform(value: string,url:string = ''): any {
+    return this._domSanitizer.bypassSecurityTrustResourceUrl( url + value );
+  }
+}
